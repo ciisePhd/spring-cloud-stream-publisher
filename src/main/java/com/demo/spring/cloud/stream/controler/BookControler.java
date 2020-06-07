67ca59664be34aebb813c88ad0f17f0ed0482f73
atalhaoui mohammed <mohammed.atalhaoui@gmail.com>
2020-06-06 21:40:24 -0400

    initial commit

---
+++ src/main/java/com/demo/spring/cloud/stream/controler/BookControler.java
@@ -0,0 +1,28 @@
+package com.demo.spring.cloud.stream.controler;
+
+
+import com.demo.spring.cloud.stream.model.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@EnableBinding(Source.class)
+public class BookControler {
+
+    @Autowired
+    private MessageChannel output;
+
+    @PostMapping("/publish")
+    public Book publishEvent(@RequestBody Book book) {
+        output.send(MessageBuilder.withPayload(book).build());
+
+        return book;
+    }
+}
---
+++ src/main/java/com/demo/spring/cloud/stream/model/Book.java
@@ -0,0 +1,15 @@
+package com.demo.spring.cloud.stream.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+
+    private int id;
+    private String name;
+
+}
